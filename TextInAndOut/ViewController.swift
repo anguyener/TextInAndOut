75fd8739c0cb4470e0060460b1e1d268ccf48438
Anna Nguyen <anguyen4@udallas.edu>
2017-10-24 16:02:55 -0500

    Initial Commit

---
+++ TextInAndOut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextInAndOut
+//
+//  Created by Anna Nguyen on 10/24/17.
+//  Copyright © 2017 Anna Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
